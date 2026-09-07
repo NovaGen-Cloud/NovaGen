d717e09ea8c1f06de93bc861e3e0528050ea3be1
NovaGen-Cloud <sohan4499@gmail.com>
2026-09-07 16:37:29 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,1 @@
-import type {CapacitorConfig} from"@capacitor/cli";const config:CapacitorConfig={appId:"com.novagen.app",appName:"NovaGen",webDir:"."};export default config;
+import type {CapacitorConfig} from"@capacitor/cli";const config:CapacitorConfig={appId:"com.novagen.app",appName:"NovaGen",webDir: "www"};export default config;
